66d8e34b7128abb688865692b1a81f50d5851d7c
Júlio César <juliobento237@gmail.com>
2024-03-26 14:12:18 -0300

    styles: fixing styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={robotoMono.className}>
         <TanstackProvider>
-          <div className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-gradient-to-br from-green-400 to-blue-500 to-pink-500 to-purple-600 p-8 text-center text-gray-900 dark:text-gray-100">
+          <div className="flex h-screen min-h-screen flex-col items-center justify-center space-y-4 overflow-hidden bg-gradient-to-br from-green-400 to-blue-500 to-pink-500 to-purple-600 px-24 py-12 text-center text-gray-900 dark:text-gray-100">
             {children}
           </div>
         </TanstackProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,51 @@
 import AsideNav from '@/components/AsideNav'
+import CardRepo from '@/components/CardRepo'
+import BarChart from '@/components/Charts/BarChart'
+import CalendarChart from '@/components/Charts/CalendarChart'
 import Header from '@/components/Header'
+import LatestCommits from '@/components/LatestCommits'
 import { Separator } from '@/components/ui/separator'
+import { Activity, Calendar, CalendarDays, Pin } from 'lucide-react'
 
 export default function Home() {
   return (
-    <div className="flex rounded-lg bg-gray-100/90 backdrop-blur-lg dark:bg-gray-800/50">
+    <div className="flex h-full flex-1 rounded-lg bg-gray-100/90 backdrop-blur-lg dark:bg-gray-800/50">
       <AsideNav />
-      <div className="h-[55rem] flex-1 overflow-y-scroll px-8">
+      <div className="overflow-y-scroll rounded-lg px-8">
         <Header />
         <Separator />
-        {/* <div className="mt-14">
-            <h2 className="mb-4 flex items-center text-left text-lg font-bold">
-              <Pin className="mr-3 rotate-45" />
-              Most Recent
-            </h2>
-            <div className="flex flex-wrap gap-4 pt-4">
-              <CardRepo />
+        <div className="mt-14">
+          <h2 className="mb-4 flex items-center text-left text-lg font-bold">
+            <Pin className="mr-3 rotate-45" />
+            Most Recent
+          </h2>
+          <div className="flex flex-wrap gap-4 pt-4">
+            <CardRepo />
+          </div>
+        </div>
+        <div className="mt-14">
+          <h2 className="mb-4 flex items-center text-left text-lg font-bold">
+            <Activity className="mr-3" />
+            Activity
+          </h2>
+          <div className="flex">
+            <BarChart />
+            <div className="h-[30rem] w-full overflow-y-scroll rounded-lg bg-white p-4 shadow">
+              <h2 className="mb-4 flex items-center text-lg font-semibold">
+                <Calendar className="mr-2 text-gray-400" />
+                Latest commits
+              </h2>
+              <LatestCommits />
             </div>
           </div>
-          <div className="mt-14">
-            <h2 className="mb-4 flex items-center text-left text-lg font-bold">
-              <Activity className="mr-3" />
-              Activity
-            </h2>
-            <div className="flex">
-              <BarChart />
-              <div className="h-[30rem] w-full overflow-y-scroll rounded-lg bg-white p-4 shadow">
-                <h2 className="mb-4 flex items-center text-lg font-semibold">
-                  <Calendar className="mr-2 text-gray-400" />
-                  Latest commits
-                </h2>
-                <LatestCommits />
-              </div>
-            </div>
-          </div>
-          <div className="mt-14">
-            <h2 className="mb-4 flex items-center text-left text-lg font-bold">
-              <CalendarDays className="mr-3" />
-              Contributions
-            </h2>
-            <CalendarChart />
-          </div> */}
+        </div>
+        <div className="mt-14">
+          <h2 className="flex items-center text-left text-lg font-bold">
+            <CalendarDays className="mr-3" />
+            Contributions
+          </h2>
+          <CalendarChart />
+        </div>
       </div>
     </div>
   )
