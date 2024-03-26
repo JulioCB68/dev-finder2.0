aa47c58cda77dc7741424b14abf705cd152e599b
Júlio César <juliobento237@gmail.com>
2024-03-26 14:10:40 -0300

    fix: fixing route protection

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,18 +4,25 @@ import { useQuery } from '@tanstack/react-query'
 import { useRouter, useSearchParams } from 'next/navigation'
 
 import { getGithubUser } from '@/services/github'
+import { parseCookies } from 'nookies'
+import { useEffect } from 'react'
 
 export default function Auth() {
+  const router = useRouter()
+
+  const user = parseCookies().user
+
   const searchParams = useSearchParams()
   const code = searchParams.get('code')
 
-  const { data: user } = useQuery({
+  const { data } = useQuery({
     queryKey: ['githubToken'],
     queryFn: () => getGithubUser(code as string),
     retry: false,
+    enabled: !!user,
   })
 
-  const router = useRouter()
-
-  router.replace('/')
+  useEffect(() => {
+    setTimeout(() => router.replace('/'), 450)
+  })
 }
