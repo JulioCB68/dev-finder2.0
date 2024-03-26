faf4bfeef8da94bfc49412a7b1b4158d21ad4c5f
Júlio César <juliobento237@gmail.com>
2024-03-26 14:27:57 -0300

    fix: fixing auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,13 +4,10 @@ import { useQuery } from '@tanstack/react-query'
 import { useRouter, useSearchParams } from 'next/navigation'
 
 import { getGithubUser } from '@/services/github'
-import { parseCookies } from 'nookies'
 import { useEffect } from 'react'
 
 export default function Auth() {
   const router = useRouter()
-
-  const user = parseCookies().user
 
   const searchParams = useSearchParams()
   const code = searchParams.get('code')
@@ -19,10 +16,9 @@ export default function Auth() {
     queryKey: ['githubToken'],
     queryFn: () => getGithubUser(code as string),
     retry: false,
-    enabled: !!user,
   })
 
   useEffect(() => {
-    setTimeout(() => router.replace('/'), 450)
+    setTimeout(() => router.replace('/'), 500)
   })
 }
